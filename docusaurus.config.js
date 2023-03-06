111caf226f6e3d0ef6c3e08e2f6d2391fda46ae2
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2023-03-06 23:31:22 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,11 +55,36 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       algolia: {
-        appId: 'FN0UVM57MW',
-        apiKey: '<YOUR_ALGOLIA_SEARCH_API_KEY>',
-        siteId: '592ff0d4-fe1e-4932-97f9-ee402dbd842a',
-        branch: 'main',
-        selector: 'div#search',
+        // The application ID provided by Algolia
+      appId: 'FN0UVM57MW',
+
+      // Public API key: it is safe to commit it
+      apiKey: '35a6107282bbb88203f03f97f52a9708',
+
+      indexName: 'Crawler Netlify - seed-of-hope',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+//       replaceSearchResultPathname: {
+//         from: '/docs/', // or as RegExp: /\/docs\//
+//         to: '/',
+//       },
+
+      // Optional: Algolia search parameters
+//       searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      //... other Algolia params
+        iteId: '592ff0d4-fe1e-4932-97f9-ee402dbd842a',
+    branch: 'main',
+    selector: 'div#search',
       },
       navbar: {
         title: "Seed of Hope",
