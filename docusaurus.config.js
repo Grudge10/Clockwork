b417a662375906864f2baae7f0b1881eabe0d10a
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2023-03-12 13:45:01 +0800

    Added working Search Bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,6 +80,29 @@ const config = {
           },
         ],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "12NX5LCAT8",
+
+        // Public API key: it is safe to commit it
+        apiKey: "9fd64549423038fa3df069cdae973c7e",
+
+        indexName: "seed-of-hope",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: "external\\.com|domain\\.com",
+
+        // // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        // // Optional: path for search page that enabled by default (`false` to disable it)
+        // searchPagePath: "search",
+
+        //... other Algolia params
+      },
       footer: {
         style: "dark",
         links: [
